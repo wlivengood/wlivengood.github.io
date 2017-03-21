8b2768f45985577862bb06a90034319ef033157b
Will Livengood <wlivengo@gmail.com>
2017-03-21 18:37:19 -0400

    changed about me section, added short circuiting of typing, reorganized file structure

--- public/main.js
+++ public/main.js
@@ -1,29 +1,14 @@
-var introLines = [`^1500Hi there!`, `^1500I am a creative, passionate software engineer who 
-enjoys building robust web applications using clean, modular code across the stack.`, 
-`^1500When I'm not coding (and sometimes when I am), I like to eat good barbecue, sip single malt 
-scotch, play my trumpet and worry far more than is reasonable about fantasy football.`,
-`^1500Have a look around!`];
+var introLines = [`^1500Hi there!^500 I'm Will.`, `^1500I write software.`, 
+`^1500I love clean, modular code, single malt scotch, barbecue, fantasy football and playing
+ my trumpet.`, `^1500Have a look around!`];
 
 $(document).ready(function() {
+    // Stop the typing on a click
+    $(".about").click(shortCircuit);
 
-    var nextLine = function(line) {
-        return function() {
-            if (line < introLines.length) {
-                $(".typed-cursor").fadeOut("fast");
-                $(".intro-text-" + line).typed({
-                    strings: [introLines[line]],
-                    typeSpeed: 0,
-                    cursorChar: " |",
-                    callback: nextLine(line + 1)
-                })
-            }
-            else {
-                $(".after-intro").fadeIn("slow");
-                $(".copyright").fadeIn("slow");
-            }
-        }
-    };
-
+    /* Hide everything, bring in the header, then start typing and bring in each successive
+    * line recursively */
+    $(".short-circuit").hide();
     $(".after-intro").hide();
     $(".copyright").hide();
     $(".header").hide().slideDown('slow', function() {
@@ -37,3 +22,27 @@ $(document).ready(function() {
 });
 
 
+ function shortCircuit(e) {
+    $(".intro-text-0").parent().hide();
+    $(".short-circuit").fadeIn("fast");
+    $(".after-intro").fadeIn("slow");
+    $(".copyright").fadeIn("slow");
+ }
+
+function nextLine(line) {
+     return function() {
+         if (line < introLines.length) {
+             $(".typed-cursor").fadeOut("fast");
+             $(".intro-text-" + line).typed({
+                 strings: [introLines[line]],
+                 typeSpeed: 0,
+                 cursorChar: " |",
+                 callback: nextLine(line + 1)
+             })
+         }
+         else {
+             $(".after-intro").fadeIn("slow");
+             $(".copyright").fadeIn("slow");
+         }
+     }
+ };
